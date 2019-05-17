9d6746d64ac31ee1fefc3597f9aa15cc123437c5
CHESERGIU <sergiu.chereches@gmail.com>
2019-05-17 13:19:40 +0300

    initial commit

---
+++ JSONNumbers/JSONNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSONNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
